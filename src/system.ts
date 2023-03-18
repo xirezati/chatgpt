8badab390b8f91ca51828e758863d511600b234d
xirezati <78945872+xirezati@users.noreply.github.com>
2023-03-19 05:11:47 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -12,7 +12,7 @@ export const message = `- 由 [OpenAI API (gpt-3.5-turbo)](https://platform.open
 - 您在使用的过程中，应遵守所在地相关法规政策、用户协议、规则规范。
 - 如果本项目对你有所帮助，可以给小猫 [买点零食](https://cdn.jsdelivr.net/gh/ourongxing/chatgpt-vercel/assets/reward.gif)，但不接受任何付费功能请求。
 - 如果要选择人物预设 请按[[空格]]，电脑上换行请按[[Shift]] + [[Enter]]，出现报错请点右下角清空对话。
-- 如要买私人版，请联系微信：lIIlIIlIIIlIIlIIIll
+- 111
 
 export type Setting = typeof setting
 

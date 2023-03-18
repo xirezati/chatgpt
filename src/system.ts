cb0f4e72b9b89a7f47947aee5729f89c7bed2573
xirezati <78945872+xirezati@users.noreply.github.com>
2023-03-19 05:42:45 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -12,8 +12,7 @@ export const message = `- 由 [OpenAI API (gpt-3.5-turbo)](https://platform.open
 - 公益网站访问费用由XirZati承担，请勿频繁访问，哪天没钱了就关闭了，推荐用私人版。
 - 您在使用的过程中，应遵守所在地相关法规政策、用户协议、规则规范。
 - 如果要选择人物预设 请按[[空格]]，电脑上换行请按[[Shift]] + [[Enter]]，出现报错请点右下角清空对话。
-- ChatGPT是一个超强的AI，它会创作、写论文、答辩、编程等
-- <span class="text-transparent font-extrabold bg-clip-text bg-gradient-to-r dark:from-yellow-300 from-yellow-600 dark:to-red-700 to-red-700 mr-1">ChatGPT</span>
+- ChatGPT是一个超强的AI，它会创作、写论文、答辩、编程等。
 - 如要买私人版，请联系微信：lIIlIIlIIIlIIlIIIll`
 
 export type Setting = typeof setting

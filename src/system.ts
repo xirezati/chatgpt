088eb9ec960447d9e16fd75823f36c09e24f4ff2
xirezati <78945872+xirezati@users.noreply.github.com>
2023-03-19 05:28:53 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -9,10 +9,11 @@ export const setting = {
 
 export const message = `- 由 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat)提供支持。
 - 由 [XirZati](https://www.xirzati.icu) 基于 [chatgpt-demo](https://github.com/ddiu8081/chatgpt-demo) 开发
+- <b>公益网站访问费用由XirZati承担，请勿频繁访问，哪天没钱了就关闭了，推荐用私人版。</b>
 - 您在使用的过程中，应遵守所在地相关法规政策、用户协议、规则规范。
 - 如果要选择人物预设 请按[[空格]]，电脑上换行请按[[Shift]] + [[Enter]]，出现报错请点右下角清空对话。
-- 如要买私人版，请联系微信：lIIlIIlIIIlIIlIIIll
-- [[Shift]] + [[Enter]] 换行。开头输入 [[/]] 或者 [[空格]] Prompt 预设。[[↑]] 可编辑最近一次提问。点击名称滚动到顶部，点击输入框滚动到底部。`
+- ChatGPT是一个超强的AI，它会创作、写论文、答辩、编程等
+- 如要买私人版，请联系微信：lIIlIIlIIIlIIlIIIll`
 
 export type Setting = typeof setting
 

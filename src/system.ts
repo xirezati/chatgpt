a4c88070a0bdc8ff6f060cd00658feacee51b541
xirezati <78945872+xirezati@users.noreply.github.com>
2023-03-19 05:06:16 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -11,7 +11,7 @@ export const message = `- 由 [OpenAI API (gpt-3.5-turbo)](https://platform.open
 - 由 [XirZati](https://www.xirzati.icu/)开发
 - 访问费用由XirZati承担，请勿频繁访问。
 - 您在使用的过程中，应遵守所在地相关法规政策、用户协议、规则规范。
-- 如果要选择人物预设 请按[[空格]]，电脑上换行请按 [[Shift]] + [[Enter]]，出现报错请点右下角清空对话。
+- 如果要选择人物预设 请按[[空格]]，电脑上换行请按[[Shift]] + [[Enter]]，出现报错请点右下角清空对话。
 - 如要买私人版，请联系微信：lIIlIIlIIIlIIlIIIll
 
 export type Setting = typeof setting

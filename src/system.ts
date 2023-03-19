a5788a63c0e14133c56ad44c97c4fabcdcc2251c
xirezati <78945872+xirezati@users.noreply.github.com>
2023-03-19 19:29:38 +0800

    Update system.ts

--- src/system.ts
+++ src/system.ts
@@ -8,7 +8,7 @@ export const setting = {
 }
 
 export const message = `- 由 [OpenAI API (gpt-3.5-turbo)](https://platform.openai.com/docs/guides/chat)提供支持。
-- 由 [XirZati](https://www.xirzati.icu) 基于 [chatgpt-demo](https://github.com/ddiu8081/chatgpt-demo) 开发
+- 由 [XirZati](https://www.xirzati.icu) 基于 [chatgpt-demo](https://raw.githubusercontent.com/xirezati/images/main/mm_facetoface_collect_qrcode_1679224023713.png) 开发
 - 公益网站访问费用由XirZati承担，请勿频繁访问，哪天没钱了就关闭了，推荐用私人版。
 - 您在使用的过程中，应遵守所在地相关法规政策、用户协议、规则规范。
 - 如果要选择人物预设 请按[[空格]]，电脑上换行请按[[Shift]] + [[Enter]]，出现报错请点右下角清空对话。
